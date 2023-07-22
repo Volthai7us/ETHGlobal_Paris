26127bfebb1840239f3b1bca7bf4cb1d717376b8
Emir Soyturk <emirsytrk@gmail.com>
2023-07-22 04:01:38 +0300

    ui updates

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 function Navbar() {
   return (
     <div className="flex items-center justify-between bg-darkGreen p-4">
-      <div className="text-white font-bold text-xl">Orderbook App</div>
+      <div className="text-white font-bold text-xl">Trusty Order</div>
       <div className="flex space-x-4">
         {/* <button className="bg-lightGreen text-darkGreen rounded-md p-2 transform hover:scale-105 transition-transform">
           Wallet Connect
